94e3f5a6ef53e498fc736a321df0be39bbdb682c
Dmitry Sharabin <dmitrysharabin@gmail.com>
2018-10-10 18:40:50 +0300

    Project Requirements for Cat Clicker Premium
    Visuals
    The application should display
    
    a list of at least 5 cats, listed by name
    an area to display the selected cat
    In the cat display area, the following should be displayed
    
    the cat's name
    a picture of the cat
    text showing the number of clicks
    The specifics of the layout do not matter, so style it however you'd 
    like.

--- js/app.js
+++ js/app.js
@@ -4,16 +4,46 @@ new Vue({
     cats: [{
       name: 'Tom',
       clickCounter: 0,
-      src: './images/tom.jpg'
+      src: './images/tom.jpg',
+      visible: false
     }, {
       name: 'Alice',
       clickCounter: 0,
-      src: './images/alice.jpg'
+      src: './images/alice.jpg',
+      visible: false
+    },
+    {
+      name: 'Tigger',
+      clickCounter: 0,
+      src: './images/tigger.jpg',
+      visible: false
+    }, {
+      name: 'Kitty',
+      clickCounter: 0,
+      src: './images/kitty.jpg',
+      visible: false
+    }, {
+      name: 'Oliver',
+      clickCounter: 0,
+      src: './images/oliver.jpg',
+      visible: false
+    }, {
+      name: 'Bella',
+      clickCounter: 0,
+      src: './images/bella.jpg',
+      visible: false
     }]
   },
   methods: {
     incCounter(index) {
       this.cats[index].clickCounter += 1;
+    },
+
+    makeVisible(index) {
+      this.cats.forEach(cat => {
+        cat.visible = false;
+      });
+      this.cats[index].visible = true;
     }
   }
 });
